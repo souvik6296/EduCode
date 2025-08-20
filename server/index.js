d516d6517b9b3c8b9ea1fa16f446c443dbd7627a
Souvik Gupta <souvikgupta64@gmail.com>
2025-08-20 10:34:16 +0530

    user update

--- server/index.js
+++ server/index.js
@@ -251,7 +251,7 @@ app.post('/createToken', async (req, res) => {
 
 app.get("/teachers/getStudentList/:teacherId", async (req, res) => {
     const { teacherId } = req.params;
-    const data = {studentList: ["UNI001", "UNI002"]};
+    const data = {studentList: ["UNI001", "UNI002", "UNI005"]};
     if (!data.success) {
         return res.status(400).send(data);
     } else if (data.success) {
