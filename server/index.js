de3ac81bc65e849dd0ad3ba358d091c6a0a7bfe2
Souvik Gupta <souvikgupta64@gmail.com>
2025-08-26 13:32:48 +0530

    session manage system fix 4

--- server/index.js
+++ server/index.js
@@ -201,7 +201,7 @@ app.post("/students/test-result-status", handleGetTestResultStatus); // Get test
 app.post("/students/upload-image", uploadMiddleware.single("image"), handleUploadStudentImage); // Upload student image and get download URL
 app.post("/students/resume-test", handleResumeTest); // Resume test and save last submissions
 app.post("/students/save-mcq-submission", handleSaveMCQSubmission); // Save MCQ submission
-app.post("/students/verify-session", handleVerifyStudent); // Verify student session
+app.get("/students/verify-session", handleVerifyStudent); // Verify student session
 // Gemini AI Chatbot endpoint
 app.post("/students/gemini-chat", handleGeminiChat); // Chat with Gemini AI
 
